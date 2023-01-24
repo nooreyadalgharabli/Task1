8bc5b0b31dbd5bb335a5c36363cbde1aab984f4e
Noor <noor2159805@gmail.com>
2023-01-24 14:34:11 +0200

    initial commit

---
+++ test.js
@@ -0,0 +1,52 @@
+let userList = [
+    {
+      id: 1,
+      firstName: 'Mohammed',
+      lastName: 'Ahmed',
+      email: 'Mohammed@gmail.com',
+      balance: 450,
+      isAdmin: true
+    },
+    {
+      id: 2,
+      firstName: 'Ali',
+      lastName: 'Omar',
+      email: 'Ali@gmail.com',
+      balance: 750,
+      isAdmin: true
+    },
+    {
+      id: 3,
+      firstName: 'Abdallah',
+      lastName: 'mahmoud',
+      email: 'Abdallah@gmail.com',
+      balance: 500,
+      isAdmin: false
+    },
+  ]
+//add new user
+const newUser = {
+    id: 4,
+    firstName: 'Noor',
+    lastName: 'Eyad',
+    email: 'Noor@gmail.com',
+    balance: 550,
+    isAdmin: true
+  }
+
+const pushNewUser = userList.push(newUser);
+
+//edit the name of the user which is 2 to be 'Hazem'
+userList[1].firstName="Hazem";
+
+
+//delete all users who are not admin
+
+
+for(let i =0; i<userList.length;++i){
+    if (userList[i].isAdmin == false){
+        delete userList[i];
+    }
+}
+
+console.log(userList);
